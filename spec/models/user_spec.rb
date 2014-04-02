ded29faf43baa0f736bb372398b821a22011d2ff
Eben Lowe <eben.lowe@gmail.com>
2014-04-01 22:54:49 -0400

    Added minimal unit tests for user spec.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe User do
+
+  describe "Database Tests" do
+    it { should have_db_column(:email).of_type(:string) }
+    it { should have_db_column(:encrypted_password).of_type(:string) }
+  end
+
+  describe "Validation Tests" do
+    it { should have_valid(:email).when("foob@example.net") }
+    it { should_not have_valid(:email).when(nil, "", "heather") }
+  end
+
+  describe "Association Tests" do
+  end
+
+end
